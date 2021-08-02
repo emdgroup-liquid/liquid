d8150e50d506a3590775182b4a941fa69170a424
Boris Diakur <borisdiakur@gmail.com>
2021-08-02 11:56:04 +0200

    ci(release): setup release jobs for release candidates on rc branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,12 +1,5 @@
 module.exports = {
-  branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
-    'main',
-    'next',
-    'next-major',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true },
-  ],
+  branches: ['main', { name: 'rc', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
