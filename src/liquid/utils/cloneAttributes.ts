caf5270dbbf566754b5fa84ea63377fee5597c40
René Schubert <reneschubert@gmx.de>
2021-10-14 17:51:37 +0200

    feat(cloneAttributes): test for name, exclude part

--- src/liquid/utils/cloneAttributes.ts
+++ src/liquid/utils/cloneAttributes.ts
@@ -2,12 +2,12 @@ import { JSXBase } from '@stencil/core/internal'
 
 export const cloneAttributes = <B extends HTMLElement>(el: B) =>
   Object.values(el.attributes).reduce<JSXBase.HTMLAttributes<B>>(
-    (acc, attr) => {
-      if (['style', 'id', 'class', 'slot'].includes(attr.name)) {
+    (acc, { name, value }) => {
+      if (!name || ['style', 'id', 'class', 'slot', 'part'].includes(name)) {
         return acc
       }
 
-      acc[attr.name] = attr.value || true
+      acc[name] = value || true
 
       return acc
     },
