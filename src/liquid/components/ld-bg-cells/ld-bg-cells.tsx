e595b2bf516c4c05fef7a12d04d7f2fdcf19dcb6
René Schubert <reneschubert@gmx.de>
2021-10-18 17:03:30 +0200

    refactor(ld-icon): use shadow DOM

--- src/liquid/components/ld-bg-cells/ld-bg-cells.tsx
+++ src/liquid/components/ld-bg-cells/ld-bg-cells.tsx
@@ -16,12 +16,14 @@ export type CellType =
 /**
  * @virtualProp ref - reference to component
  * @virtualProp {string | number} key - for tracking the node's identity when working with lists
+ * @part pattern - Element containing the cells pattern
+ * @part content - Element wrapping the slot
  */
 @Component({
   assetsDirs: ['assets'],
   tag: 'ld-bg-cells',
   styleUrl: 'ld-bg-cells.css',
-  shadow: false,
+  shadow: true,
 })
 export class LdBgCells {
   /** Cells pattern */
@@ -31,14 +33,15 @@ export class LdBgCells {
     const assetPath = getAssetPath(`./assets/${this.type}-cell.svg`)
 
     return (
-      <Host
-        class={getClassNames(['ld-bg-cells', `ld-bg-cells--${this.type}`])}
-        style={{
-          '--ld-bg-cells-image': `url(${assetPath})`,
-        }}
-      >
-        <div class="ld-bg-cells__pattern" />
-        <div class="ld-bg-cells__content">
+      <Host class={getClassNames(['ld-bg-cells', `ld-bg-cells--${this.type}`])}>
+        <div
+          class="ld-bg-cells__pattern"
+          part="pattern"
+          style={{
+            '--ld-bg-cells-image': `url(${assetPath})`,
+          }}
+        />
+        <div class="ld-bg-cells__content" part="content">
           <slot />
         </div>
       </Host>
