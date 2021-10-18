7fdd719d6fb4e7379363ab8a753910f6ffb232ac
René Schubert <reneschubert@gmx.de>
2021-10-18 14:30:06 +0200

    refactor(ld-icon): use shadow DOM

--- src/liquid/components/ld-icon/ld-icon.tsx
+++ src/liquid/components/ld-icon/ld-icon.tsx
@@ -1,4 +1,5 @@
-import { Build, Component, Host, h, Prop, State, Watch } from '@stencil/core'
+import { Build, Component, Host, h, Prop, Watch, Element } from '@stencil/core'
+import { getClassNames } from 'src/liquid/utils/getClassNames'
 import { fetchIcon } from './fetchIcon'
 
 /**
@@ -10,26 +11,27 @@ import { fetchIcon } from './fetchIcon'
   assetsDirs: ['assets'],
   tag: 'ld-icon',
   styleUrl: 'ld-icon.css',
-  shadow: false,
+  shadow: true,
 })
 export class LdIcon {
+  @Element() element: HTMLElement
+
   /** The icon name. */
   @Prop() name: string = null
 
   /** Size of the icon. */
   @Prop() size?: 'sm' | 'lg'
 
-  @State() private svg: string
-
   @Watch('name')
   private async loadIconPathData(): Promise<void> {
-    const { name } = this
-
-    if ((!Build.isBrowser && !Build.isTesting) || !name) {
+    if ((!Build.isBrowser && !Build.isTesting) || !this.name) {
       return
     }
 
-    this.svg = await fetchIcon(name)
+    const div = document.createElement('div')
+
+    div.innerHTML = await fetchIcon(this.name)
+    this.element.shadowRoot.appendChild(div.firstChild)
   }
 
   async componentWillLoad(): Promise<void> {
@@ -37,14 +39,12 @@ export class LdIcon {
   }
 
   render() {
-    let cl = 'ld-icon'
-    if (this.size) cl += ` ld-icon--${this.size}`
-
     return (
-      <Host>
-        <span class={cl} role="presentation" innerHTML={this.svg}>
-          {!this.name && <slot></slot>}
-        </span>
+      <Host
+        class={getClassNames(['ld-icon', this.size && `ld-icon--${this.size}`])}
+        role="presentation"
+      >
+        {!this.name && <slot></slot>}
       </Host>
     )
   }
