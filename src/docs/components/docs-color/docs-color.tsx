f0d38687b249eeeeee5cb39125d36d23dc41216e
Boris Diakur <borisdiakur@gmail.com>
2021-11-02 16:02:01 +0100

    docs(docs-color): include alpha value in darkness calculation

--- src/docs/components/docs-color/docs-color.tsx
+++ src/docs/components/docs-color/docs-color.tsx
@@ -26,8 +26,9 @@ export class MyComponent {
     const r = color[1]
     const g = color[2]
     const b = color[3]
+    const a = color[4]
     const hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b))
-    return hsp <= 127.5
+    return hsp <= 127.5 * (a || 1)
   }
 
   componentDidLoad() {
