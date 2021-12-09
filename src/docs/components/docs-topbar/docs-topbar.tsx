300a1bbb856371887aee17802d7e3987b31001a4
René Schubert <reneschubert@gmx.de>
2021-12-09 12:53:01 +0100

    docs(docs-topbar): use ld-header

--- src/docs/components/docs-topbar/docs-topbar.tsx
+++ src/docs/components/docs-topbar/docs-topbar.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, getAssetPath } from '@stencil/core'
+import { Component, h, getAssetPath } from '@stencil/core'
 
 /** @internal **/
 @Component({
@@ -10,51 +10,52 @@ import { Component, h, Host, getAssetPath } from '@stencil/core'
 export class DocsTopbar {
   render() {
     return (
-      <Host class="ld-theme-bubblegum docs-topbar">
-        <div class="docs-topbar__content">
-          {/*<docs-btn-dots-grid></docs-btn-dots-grid>*/}
-          <a class="docs-topbar__link-home" href="./">
-            <img
-              src={getAssetPath('./assets/logo.svg')}
-              alt=""
-              role="presentation"
-            />
-            <h1 class="docs-topbar__headline docs-topbar__headline--long">
-              <b>Liquid Oxygen</b> Documentation
-            </h1>
-            <h1 class="docs-topbar__headline docs-topbar__headline--short">
-              <b>Liquid</b> Docs
-            </h1>
-          </a>
-
-          <div class="docs-topbar__external-links">
-            <a
-              class="docs-topbar__link docs-topbar__link--figma"
-              href="https://www.figma.com/file/8GYcAOePm8Tt9qqJ7Gnv99/Liquid-Oxygen-(Share)?node-id=3%3A14310"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img
-                src={getAssetPath('./assets/figma.svg')}
-                alt="Liquid Oxygen on Figma"
-                role="presentation"
-              />
-            </a>
-            <a
-              class="docs-topbar__link docs-topbar__link--github"
-              href="https://github.com/emdgroup-liquid/liquid"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img
-                src={getAssetPath('./assets/github.svg')}
-                alt="Liquid Oxygen on GitHub"
-                role="presentation"
-              />
-            </a>
-          </div>
-        </div>
-      </Host>
+      <ld-header
+        class="docs-topbar ld-theme-bubblegum"
+        sticky
+        logo-title="Home"
+        logo-url="/"
+      >
+        <img
+          alt="Logo"
+          class="docs-topbar__logo"
+          role="presentation"
+          slot="logo"
+          src={getAssetPath('./assets/logo.svg')}
+        />
+        <p class="docs-topbar__headline docs-topbar__headline--long">
+          <b>Liquid Oxygen</b> Documentation
+        </p>
+        <p class="docs-topbar__headline docs-topbar__headline--short">
+          <b>Liquid</b> Docs
+        </p>
+        <a
+          class="docs-topbar__link"
+          href="https://www.figma.com/file/8GYcAOePm8Tt9qqJ7Gnv99/Liquid-Oxygen-(Share)?node-id=3%3A14310"
+          rel="noreferrer noopener"
+          slot="end"
+          target="_blank"
+        >
+          <img
+            src={getAssetPath('./assets/figma.svg')}
+            alt="Liquid Oxygen on Figma"
+            role="presentation"
+          />
+        </a>
+        <a
+          class="docs-topbar__link"
+          href="https://github.com/emdgroup-liquid/liquid"
+          rel="noreferrer noopener"
+          slot="end"
+          target="_blank"
+        >
+          <img
+            src={getAssetPath('./assets/github.svg')}
+            alt="Liquid Oxygen on GitHub"
+            role="presentation"
+          />
+        </a>
+      </ld-header>
     )
   }
 }
