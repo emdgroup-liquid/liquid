6646a087b7ca04f2e5b14d62ae47aa69940695fa
René Schubert <reneschubert@gmx.de>
2021-12-15 18:47:26 +0100

    docs(docs-topbar): fix docs header

--- src/docs/components/docs-topbar/docs-topbar.tsx
+++ src/docs/components/docs-topbar/docs-topbar.tsx
@@ -10,30 +10,27 @@ import { Component, h, getAssetPath } from '@stencil/core'
 export class DocsTopbar {
   render() {
     return (
-      <ld-header
-        class="docs-topbar ld-theme-bubblegum"
-        logo-title="Home"
-        logo-url="/"
-        sticky
-      >
-        <img
-          alt="Logo"
-          class="docs-topbar__logo"
-          role="presentation"
-          slot="logo"
-          src={getAssetPath('./assets/logo.svg')}
-        />
-        <p class="docs-topbar__headline docs-topbar__headline--long">
-          <b>Liquid Oxygen</b> Documentation
-        </p>
-        <p class="docs-topbar__headline docs-topbar__headline--short">
-          <b>Liquid</b> Docs
-        </p>
+      <ld-header class="docs-topbar ld-theme-bubblegum" sticky>
+        <a href="/" title="Home">
+          <img
+            alt="Logo"
+            class="docs-topbar__logo"
+            role="presentation"
+            src={getAssetPath('./assets/logo.svg')}
+          />
+        </a>
+        <a class="docs-topbar__headlines" href="/" title="Home">
+          <p class="docs-topbar__headline docs-topbar__headline--long">
+            <b>Liquid Oxygen</b> Documentation
+          </p>
+          <p class="docs-topbar__headline docs-topbar__headline--short">
+            <b>Liquid</b> Docs
+          </p>
+        </a>
         <a
           class="docs-topbar__link"
           href="https://www.figma.com/file/8GYcAOePm8Tt9qqJ7Gnv99/Liquid-Oxygen-(Share)?node-id=3%3A14310"
           rel="noreferrer noopener"
-          slot="end"
           target="_blank"
         >
           <img
@@ -46,7 +43,6 @@ export class DocsTopbar {
           class="docs-topbar__link"
           href="https://github.com/emdgroup-liquid/liquid"
           rel="noreferrer noopener"
-          slot="end"
           target="_blank"
         >
           <img
