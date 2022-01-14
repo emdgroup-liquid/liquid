7b7f044a90b47b8217324596c18e5be30cf342ca
René Schubert <reneschubert@gmx.de>
2022-01-14 17:34:09 +0100

    fix(react): add alternative approach for setting the asset path
    Co-authored-by: Boris Diakur <borisdiakur@gmail.com>

---
+++ src/liquid/utils/getLdAssetPath.ts
@@ -0,0 +1,26 @@
+import { getAssetPath } from '@stencil/core'
+
+/**
+ * Reads asset path config from a global variable, if available,
+ * and uses this instead of Stencil's getAssetPath function.
+ *
+ * This is a workaround until the following issue is resolved:
+ * https://github.com/ionic-team/stencil-ds-output-targets/issues/186
+ */
+export const getLdAssetPath = (path: string) => {
+  if (typeof window !== 'undefined' && window.__LD_ASSET_PATH__) {
+    let finalPath = path
+
+    if (path.startsWith('./')) {
+      finalPath = path.substring(2)
+    }
+
+    if (!window.__LD_ASSET_PATH__.endsWith('/')) {
+      finalPath = '/' + finalPath
+    }
+
+    return window.__LD_ASSET_PATH__ + finalPath
+  }
+
+  return getAssetPath(path)
+}
