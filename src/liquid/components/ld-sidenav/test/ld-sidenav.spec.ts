74ba4baa46f821e26824ae82247056337eae90e8
Boris Diakur <borisdiakur@gmail.com>
2022-04-06 15:00:10 +0200

    fix(ld-sidenav): abbreviation with special characters

--- src/liquid/components/ld-sidenav/test/ld-sidenav.spec.ts
+++ src/liquid/components/ld-sidenav/test/ld-sidenav.spec.ts
@@ -13,8 +13,8 @@ import { LdSidenavHeading } from '../ld-sidenav-heading/ld-sidenav-heading'
 import { LdSidenavScrollerInternal } from '../ld-sidenav-scroller-internal/ld-sidenav-scroller-internal'
 import { LdTooltip } from '../../ld-tooltip/ld-tooltip'
 import { getFirstFocusable } from '../../../utils/focus'
-import '../../../utils/mutationObserver'
 import { getSidenavWithSubnavigation } from './utils'
+import '../../../utils/mutationObserver'
 
 let matchMedia
 const mockedGetFirstFocusable = getFirstFocusable as jest.Mock
@@ -1290,6 +1290,74 @@ describe('ld-sidenav', () => {
       await page.waitForChanges()
 
       expect(button2.focus).toHaveBeenCalled()
+    })
+  })
+
+  describe('navitem abbreviation', () => {
+    it('should abbreviate to one character if M is included', async () => {
+      const page = await newSpecPage({
+        components: [LdSidenav, LdSidenavSlider, LdSidenavNavitem, LdTooltip],
+        html: `<ld-sidenav open>
+            <ld-sidenav-slider label="Outline of Computer Science">
+              <ld-sidenav-navitem>
+                Mathematical foundations
+              </ld-sidenav-navitem>
+            </ld-sidenav-slider>
+          </ld-sidenav>
+        `,
+      })
+      await page.waitForChanges()
+
+      const ldSidenavNavitem = page.root.querySelector('ld-sidenav-navitem')
+      const abbr = ldSidenavNavitem.shadowRoot.querySelector(
+        '.ld-sidenav-navitem__abbr'
+      )
+
+      expect(abbr.textContent).toEqual('M')
+    })
+
+    it('should abbreviate to two character if M is not included', async () => {
+      const page = await newSpecPage({
+        components: [LdSidenav, LdSidenavSlider, LdSidenavNavitem, LdTooltip],
+        html: `<ld-sidenav open>
+            <ld-sidenav-slider label="Outline of Computer Science">
+              <ld-sidenav-navitem>
+                Artificial intelligence
+              </ld-sidenav-navitem>
+            </ld-sidenav-slider>
+          </ld-sidenav>
+        `,
+      })
+      await page.waitForChanges()
+
+      const ldSidenavNavitem = page.root.querySelector('ld-sidenav-navitem')
+      const abbr = ldSidenavNavitem.shadowRoot.querySelector(
+        '.ld-sidenav-navitem__abbr'
+      )
+
+      expect(abbr.textContent).toEqual('AI')
+    })
+
+    it('should abbreviate and ignore special characters', async () => {
+      const page = await newSpecPage({
+        components: [LdSidenav, LdSidenavSlider, LdSidenavNavitem, LdTooltip],
+        html: `<ld-sidenav open>
+            <ld-sidenav-slider label="Outline of Computer Science">
+              <ld-sidenav-navitem>
+                Algorithms & data structures
+              </ld-sidenav-navitem>
+            </ld-sidenav-slider>
+          </ld-sidenav>
+        `,
+      })
+      await page.waitForChanges()
+
+      const ldSidenavNavitem = page.root.querySelector('ld-sidenav-navitem')
+      const abbr = ldSidenavNavitem.shadowRoot.querySelector(
+        '.ld-sidenav-navitem__abbr'
+      )
+
+      expect(abbr.textContent).toEqual('AD')
     })
   })
 })
