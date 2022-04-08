9b58f60dce0074e57971a32f7cb91767d0577c84
Boris Diakur <borisdiakur@gmail.com>
2022-04-08 20:24:41 +0200

    feat(ld-card): implement card component

---
+++ src/liquid/components/ld-card/test/ld-card.spec.tsx
@@ -0,0 +1,49 @@
+import { newSpecPage } from '@stencil/core/testing'
+import { LdCard } from '../ld-card'
+
+describe('ld-card', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [LdCard],
+      html: `<ld-card>Hello</ld-card>`,
+    })
+    expect(page.root).toMatchSnapshot()
+  })
+
+  it('renders with li tag', async () => {
+    const page = await newSpecPage({
+      components: [LdCard],
+      html: `<ld-card tag="li">Hello</ld-card>`,
+    })
+    expect(page.root).toMatchSnapshot()
+  })
+
+  describe('adds classes according to props', () => {
+    it('adds size class', async () => {
+      const page = await newSpecPage({
+        components: [LdCard],
+        html: `<ld-card size="sm">Hello</ld-card>`,
+      })
+      const card = page.root.shadowRoot.querySelector('.ld-card')
+      expect(card).toHaveClass('ld-card--sm')
+    })
+
+    it('adds shadow class', async () => {
+      const page = await newSpecPage({
+        components: [LdCard],
+        html: `<ld-card shadow="sticky">Hello</ld-card>`,
+      })
+      const card = page.root.shadowRoot.querySelector('.ld-card')
+      expect(card).toHaveClass('ld-card--sticky')
+    })
+
+    it('adds shadow interactive class', async () => {
+      const page = await newSpecPage({
+        components: [LdCard],
+        html: `<ld-card shadow-interactive="sticky">Hello</ld-card>`,
+      })
+      const card = page.root.shadowRoot.querySelector('.ld-card')
+      expect(card).toHaveClass('ld-card--interactive-sticky')
+    })
+  })
+})
