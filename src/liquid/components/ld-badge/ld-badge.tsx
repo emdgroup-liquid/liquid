ef531ca4336002c319c93ff160bfa4185235d862
Ion Taban <ion.taban@plentific.com>
2022-05-16 22:49:36 +0300

    feat(ld-badge): add badge component

---
+++ src/liquid/components/ld-badge/ld-badge.tsx
@@ -0,0 +1,71 @@
+import { Component, Element, h, Host, Prop, State } from '@stencil/core'
+import { HTMLStencilElement } from '@stencil/core/internal'
+import { getClassNames } from 'src/liquid/utils/getClassNames'
+
+/**
+ * @virtualProp ref - reference to component
+ * @virtualProp {string | number} key - for tracking the node's identity when working with lists
+ */
+@Component({
+  assetsDirs: ['assets'],
+  tag: 'ld-badge',
+  styleUrl: 'ld-badge.css',
+  shadow: true,
+})
+export class LdBadge {
+  @Element() el: HTMLStencilElement
+
+  private observer: MutationObserver
+
+  /** Defines badge custom color */
+  @Prop() brandColor?: boolean
+
+  /** Icon name. */
+  @Prop() icon?: string
+
+  /** The size of the badge, translated in rem */
+  @Prop() size?: 'lg'
+
+  @State() hasCustomIcon = false
+  @State() hasText = false
+
+  private updateState = () => {
+    this.hasCustomIcon = !!this.el.querySelector('[slot="icon"]')
+    this.hasText = !!this.el.textContent.trim()
+  }
+
+  componentWillLoad() {
+    this.observer = new MutationObserver(this.updateState)
+    this.observer.observe(this.el, {
+      subtree: true,
+      childList: true,
+      attributes: false,
+    })
+
+    this.updateState()
+  }
+
+  disconnectedCallback() {
+    if (this.observer) this.observer.disconnect()
+  }
+
+  render() {
+    const cl = getClassNames([
+      'ld-badge',
+      this.size && `ld-badge--${this.size}`,
+      this.hasText && 'ld-badge--with-text',
+      this.brandColor && `ld-badge--brand-color`,
+    ])
+
+    return (
+      <Host class={cl}>
+        {(this.icon || this.hasCustomIcon) && (
+          <ld-icon class="ld-badge__icon" part="icon" name={this.icon}>
+            <slot name="icon"></slot>
+          </ld-icon>
+        )}
+        <slot></slot>
+      </Host>
+    )
+  }
+}
---
+++ src/liquid/components/ld-badge/test/ld-badge.e2e.ts
@@ -0,0 +1,230 @@
+import {
+  analyzeAccessibility,
+  getPageWithContent,
+} from 'src/liquid/utils/e2e-tests'
+import { LdIcon } from '../../ld-icon/ld-icon'
+import { LdBadge } from '../ld-badge'
+
+describe('ld-badge', () => {
+  describe('web component', () => {
+    it('renders', async () => {
+      const page = await getPageWithContent(
+        '<ld-badge icon="checkmark"></ld-badge>'
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with custom icon', async () => {
+      const page = await getPageWithContent(
+        `<ld-badge>
+            <svg slot="icon" viewBox="0 0 14 14">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        </ld-badge>`
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with with size lg', async () => {
+      const page = await getPageWithContent(
+        '<ld-badge icon="checkmark" size="lg"></ld-badge>'
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with text', async () => {
+      const page = await getPageWithContent('<ld-badge>Badge</ld-badge>')
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with large text', async () => {
+      const page = await getPageWithContent(
+        `<ld-badge size="lg">Badge</ld-badge>`
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with icon and text', async () => {
+      const page = await getPageWithContent(
+        '<ld-badge icon="checkmark" >Badge</ld-badge>'
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with large icon and text', async () => {
+      const page = await getPageWithContent(
+        '<ld-badge icon="checkmark"  size="lg">Badge</ld-badge>'
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with line break', async () => {
+      const page = await getPageWithContent(
+        `<ld-badge icon="checkmark">
+          Badge with a<br>line break
+        </ld-badge>`
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders on brand color', async () => {
+      const page = await getPageWithContent(
+        '<ld-badge icon="checkmark" brand-color>Badge</ld-badge>',
+        {
+          bgColor: 'var(--ld-thm-primary)',
+        }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with custom color', async () => {
+      const page = await getPageWithContent(
+        `<ld-badge
+          style="--ld-badge-bg-col: var(--ld-thm-success)"
+          icon="checkmark">
+          Badge
+        </ld-badge>`
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+  })
+
+  describe('css component', () => {
+    it('renders', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>`,
+        { components: [LdBadge, LdIcon] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with with size lg', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge ld-badge--lg">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>`,
+        { components: [LdBadge, LdIcon] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with text', async () => {
+      const page = await getPageWithContent(
+        '<span class="ld-badge">Badge</span>',
+        { components: [LdBadge] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with large text', async () => {
+      const page = await getPageWithContent(
+        '<span class="ld-badge ld-badge--lg">Badge</span>',
+        { components: [LdBadge] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with icon and text', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge ld-badge--with-text">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          Badge
+        </span>`,
+        { components: [LdBadge, LdIcon] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with large icon and text', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge ld-badge--lg ld-badge--with-text">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          Badge
+        </span>`,
+        { components: [LdBadge, LdIcon] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with line break', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge ld-badge--with-text">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          Badge with a<br>line break
+        </span>`,
+        { components: [LdBadge, LdIcon] }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders on brand color', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge ld-badge--with-text ld-badge--brand-color">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          Badge
+        </span>`,
+        {
+          bgColor: 'var(--ld-thm-primary)',
+          components: [LdBadge, LdIcon],
+        }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+    it('renders with custom color', async () => {
+      const page = await getPageWithContent(
+        `<span
+          class="ld-badge ld-badge--with-text"
+          style="--ld-badge-bg-col: var(--ld-thm-success)">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          Badge
+        </span>`,
+        {
+          components: [LdBadge, LdIcon],
+        }
+      )
+      const results = await page.compareScreenshot()
+      expect(results).toMatchScreenshot()
+    })
+  })
+
+  describe('accessibility', () => {
+    it('is accessible as a Web Component', async () => {
+      const page = await getPageWithContent(
+        `<ld-badge icon="checkmark"></ld-badge>`
+      )
+      page.waitForChanges()
+      const accessibilityReport = await analyzeAccessibility(page)
+      expect(accessibilityReport).toHaveNoAccessibilityIssues()
+    })
+    it('is accessible as a CSS Component', async () => {
+      const page = await getPageWithContent(
+        `<span class="ld-badge">
+          <svg class="ld-icon" viewBox="0 0 14 14" fill="none">
+            <path d="m12 4-6.592 6L2 6.6396" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>`
+      )
+      page.waitForChanges()
+      const accessibilityReport = await analyzeAccessibility(page)
+      expect(accessibilityReport).toHaveNoAccessibilityIssues()
+    })
+  })
+})
