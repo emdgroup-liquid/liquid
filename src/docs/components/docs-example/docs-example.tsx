f8c8020f2c255461471d0d52dd0222d4878c280c
Boris Diakur <borisdiakur@gmail.com>
2023-01-18 16:32:07 +0100

    docs(react): examples in code viewer

--- src/docs/components/docs-example/docs-example.tsx
+++ src/docs/components/docs-example/docs-example.tsx
@@ -1,4 +1,14 @@
-import { Component, h, Host, Prop, State, Element } from '@stencil/core'
+import {
+  Component,
+  h,
+  Host,
+  Prop,
+  State,
+  Element,
+  Event,
+  EventEmitter,
+  Listen,
+} from '@stencil/core'
 import { getClassNames } from '../../../liquid/utils/getClassNames'
 
 /** @internal **/
@@ -21,6 +31,9 @@ export class DocsExample {
 
   /** CSS component markup encoded as URI component. */
   @Prop() codeCssComponent: string
+
+  /** React component markup encoded as URI component. */
+  @Prop() codeReactComponent: string
 
   /** Adds a thin border to the container. */
   @Prop() hasBorder = false
@@ -47,7 +60,10 @@ export class DocsExample {
   @State() isCodeVisible = this.opened
 
   /** Is Web Component visible (as opposed to the css component version) */
-  @State() isWebComponent = true
+  @State() codeType: 'wc' | 'css' | 'react' = 'wc'
+
+  /** Code type pick change event. */
+  @Event() pickCodeType: EventEmitter<this['codeType']>
 
   private handlePickTheme = (event: CustomEvent<string>) => {
     this.currentTheme = event.detail
@@ -57,16 +73,56 @@ export class DocsExample {
     this.isCodeVisible = event.detail
   }
 
-  private handleSwitchComponent = () => {
-    this.isWebComponent = !this.isWebComponent
-    this.isCodeVisible = true
+  @Listen('pickCodeType', {
+    target: 'window',
+  })
+  handleSwitchCode(ev: CustomEvent<this['codeType']>) {
+    if (!this.hasCodeType(ev.detail)) return
+    this.codeType = ev.detail
+    window.localStorage.setItem(
+      'liquid_docs_preferred_code_type',
+      this.codeType
+    )
   }
 
   private unescapeCode(code) {
-    return code.replaceAll(/\\{\\{/g, '{{').replaceAll(/\\}\\}/g, '}}')
+    return (
+      code
+        // lang html
+        .replaceAll(/\\{\\{/g, '{{')
+        .replaceAll(/\\}\\}/g, '}}')
+        // lang jsx
+        .replaceAll(
+          /<span class="token punctuation">{<\/span> <span class="token punctuation">{<\/span>/g,
+          '<span class="token punctuation">{</span><span class="token punctuation">{</span>'
+        )
+        .replaceAll(
+          /<span class="token punctuation">}<\/span> <span class="token punctuation">}<\/span>/g,
+          '<span class="token punctuation">}</span><span class="token punctuation">}</span>'
+        )
+    )
+  }
+
+  private hasCodeType(codeType: this['codeType']) {
+    console.info('codeType', codeType)
+    if (codeType === 'wc') {
+      return Boolean(this.el.querySelector('[slot="code"]'))
+    }
+    return Array.from(this.el.querySelectorAll('[slot^="code"]')).some(
+      (slot) =>
+        slot.getAttribute('slot').toLowerCase() === `code${codeType}component`
+    )
   }
 
   componentWillLoad() {
+    const preferredCodeType = window.localStorage.getItem(
+      'liquid_docs_preferred_code_type'
+    ) as this['codeType']
+    if (preferredCodeType) {
+      if (this.hasCodeType(preferredCodeType)) {
+        this.codeType = preferredCodeType as this['codeType']
+      }
+    }
     this.el.querySelectorAll('[slot^="code"]').forEach((slot) => {
       slot.innerHTML = this.unescapeCode(slot.innerHTML)
     })
@@ -78,9 +134,9 @@ export class DocsExample {
       this.isCodeVisible && 'docs-example--code-visible',
       this.hasBorder && 'docs-example--has-border',
       this.hasPadding && 'docs-example--has-padding',
-      this.isWebComponent
-        ? 'docs-example--web-component'
-        : ' docs-example--css-component',
+      this.codeType === 'wc' && 'docs-example--web-component',
+      this.codeType === 'css' && 'docs-example--css-component',
+      this.codeType === 'react' && 'docs-example--react-component',
     ]
 
     let clShow = 'docs-example__show'
@@ -99,13 +155,17 @@ export class DocsExample {
         </div>
         <div class="docs-example__tools-scroll-container">
           <div class="docs-example__tools">
-            {this.codeCssComponent && (
+            {(this.codeCssComponent || this.codeReactComponent) && (
               <ld-switch
-                onLdswitchchange={this.handleSwitchComponent}
+                onClick={() => (this.isCodeVisible = true)}
+                onLdswitchchange={(ev) => {
+                  this.handleSwitchCode(ev as CustomEvent<this['codeType']>)
+                  this.pickCodeType.emit(this.codeType)
+                }}
                 class="docs-example__tool-switch"
                 size="sm"
               >
-                <ld-switch-item checked={this.isWebComponent}>
+                <ld-switch-item value="wc" checked={this.codeType === 'wc'}>
                   <ld-icon
                     slot="icon-start"
                     size="sm"
@@ -119,23 +179,49 @@ export class DocsExample {
                     </svg>
                   </ld-icon>
                 </ld-switch-item>
-                <ld-switch-item checked={!this.isWebComponent}>
-                  <ld-icon
-                    slot="icon-start"
-                    size="sm"
-                    aria-label="CSS component"
+                {this.codeReactComponent && (
+                  <ld-switch-item
+                    value="react"
+                    checked={this.codeType === 'react'}
                   >
-                    <svg
-                      viewBox="0 0 800 300"
-                      style={{ transform: 'scale(1.2)' }}
+                    <ld-icon
+                      slot="icon-start"
+                      size="sm"
+                      aria-label="React component"
                     >
-                      <path
-                        fill="currentColor"
-                        d="M0 0h238.7v99.8H99.8v99.8h139v99.9H0V0zM283.2 0h235.3v85.6H381.6v17h136.9v196.9H283.2v-89.9h136.9v-17H283.2V0zM564.7 0H800v85.6H663.1v17H800v196.9H564.7v-89.9h136.9v-17H564.7V0z"
-                      />
-                    </svg>
-                  </ld-icon>
-                </ld-switch-item>
+                      <svg
+                        viewBox="-11.5 -10.2 23 20.5"
+                        style={{ transform: 'scale(1.1)' }}
+                      >
+                        <circle r="2" fill="currentColor" />
+                        <g stroke="currentColor" fill="none">
+                          <ellipse rx="11" ry="4.2" />
+                          <ellipse rx="11" ry="4.2" transform="rotate(60)" />
+                          <ellipse rx="11" ry="4.2" transform="rotate(120)" />
+                        </g>
+                      </svg>
+                    </ld-icon>
+                  </ld-switch-item>
+                )}
+                {this.codeCssComponent && (
+                  <ld-switch-item value="css" checked={this.codeType === 'css'}>
+                    <ld-icon
+                      slot="icon-start"
+                      size="sm"
+                      aria-label="CSS component"
+                    >
+                      <svg
+                        viewBox="0 0 800 300"
+                        style={{ transform: 'scale(1.2)' }}
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M0 0h238.7v99.8H99.8v99.8h139v99.9H0V0zM283.2 0h235.3v85.6H381.6v17h136.9v196.9H283.2v-89.9h136.9v-17H283.2V0zM564.7 0H800v85.6H663.1v17H800v196.9H564.7v-89.9h136.9v-17H564.7V0z"
+                        />
+                      </svg>
+                    </ld-icon>
+                  </ld-switch-item>
+                )}
               </ld-switch>
             )}
             <div class="docs-example__tool-buttons">
@@ -150,15 +236,24 @@ export class DocsExample {
           </div>
         </div>
         <div class="docs-example__code">
-          <docs-copy-to-cb
-            class="docs-example__copy-to-clipboard"
-            textToCopy={this.unescapeCode(
-              decodeURIComponent(
-                this.isWebComponent ? this.code : this.codeCssComponent
-              )
-            )}
-          />
+          <div class="docs-example__code-tools">
+            <docs-copy-to-cb
+              class="docs-example__copy-to-clipboard"
+              textToCopy={this.unescapeCode(
+                decodeURIComponent(
+                  this.codeType === 'wc'
+                    ? this.code
+                    : this.codeType === 'css'
+                    ? this.codeCssComponent
+                    : this.codeType === 'react'
+                    ? this.codeReactComponent
+                    : ''
+                )
+              )}
+            />
+          </div>
           <slot name="code"></slot>
+          <slot name="codeReactComponent"></slot>
           <slot name="codeCssComponent"></slot>
         </div>
       </Host>
