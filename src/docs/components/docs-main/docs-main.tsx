fa91b0307d6fff3bd86e94d6a1109f0a99a4ccfc
Boris Diakur <borisdiakur@gmail.com>
2023-02-13 18:46:19 +0100

    docs(docs-main): add copy-to-cb to code blocks

--- src/docs/components/docs-main/docs-main.tsx
+++ src/docs/components/docs-main/docs-main.tsx
@@ -9,18 +9,32 @@ import { Component, Element, h, Host } from '@stencil/core'
 export class DocsNav {
   @Element() el: HTMLElement
 
+  private mainRef!: HTMLElement
+
   componentDidLoad() {
-    this.el
+    this.mainRef
       .querySelectorAll('#css-variables + table code, #properties + table code')
       .forEach((code) => {
         code.innerHTML = code.innerHTML.replace(/\\\|/g, '|')
       })
+
+    // Timeout is required in order to use innerText.
+    // We use innerText and not textContent in order to preserve line breaks.
+    setTimeout(() => {
+      Array.from(this.mainRef.children)
+        .filter((child) => child.tagName === 'PRE')
+        .forEach((pre: HTMLPreElement) => {
+          const copyToCB = document.createElement('docs-copy-to-cb')
+          copyToCB.textToCopy = pre.innerText
+          pre.appendChild(copyToCB)
+        })
+    })
   }
 
   render() {
     return (
       <Host class="docs-main">
-        <main id="main">
+        <main id="main" ref={(ref) => (this.mainRef = ref)}>
           <slot></slot>
         </main>
       </Host>
