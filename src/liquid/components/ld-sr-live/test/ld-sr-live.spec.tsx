0d0c65db1bced5a67cd148597273a8a046151f80
Boris Diakur <borisdiakur@gmail.com>
2023-04-28 12:41:48 +0200

    test(ld-sr-live): alert and status message updates

--- src/liquid/components/ld-sr-live/test/ld-sr-live.spec.tsx
+++ src/liquid/components/ld-sr-live/test/ld-sr-live.spec.tsx
@@ -10,4 +10,52 @@ describe('ld-sr-live', () => {
     })
     expect(page.root).toMatchSnapshot()
   })
+
+  it('informs', async () => {
+    const page = await newSpecPage({
+      components: [LdSrLive],
+      template: () => <ld-sr-live />,
+    })
+    const statusRegion = page.root.shadowRoot.querySelector('[role="status"]')
+
+    window.dispatchEvent(
+      new CustomEvent('ldSrLiveInfo', {
+        detail: 'Your profile has been updated.',
+      })
+    )
+    await page.waitForChanges()
+    expect(statusRegion.textContent).toEqual('Your profile has been updated.​')
+
+    window.dispatchEvent(
+      new CustomEvent('ldSrLiveInfo', {
+        detail: 'Your profile has been updated.',
+      })
+    )
+    await page.waitForChanges()
+    expect(statusRegion.textContent).toEqual('Your profile has been updated.')
+  })
+
+  it('alerts', async () => {
+    const page = await newSpecPage({
+      components: [LdSrLive],
+      template: () => <ld-sr-live />,
+    })
+    const alertRegion = page.root.shadowRoot.querySelector('[role="alert"]')
+
+    window.dispatchEvent(
+      new CustomEvent('ldSrLiveAlert', {
+        detail: 'An error has occured.',
+      })
+    )
+    await page.waitForChanges()
+    expect(alertRegion.textContent).toEqual('An error has occured.​')
+
+    window.dispatchEvent(
+      new CustomEvent('ldSrLiveAlert', {
+        detail: 'An error has occured.',
+      })
+    )
+    await page.waitForChanges()
+    expect(alertRegion.textContent).toEqual('An error has occured.')
+  })
 })
