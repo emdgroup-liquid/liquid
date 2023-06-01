e473c2ceed3834fb0d6e3c1a15657f9728bf83e3
Boris Diakur <borisdiakur@gmail.com>
2023-06-01 16:15:43 +0200

    fix(ld-card): throws if there is no parent element

--- src/liquid/components/ld-card/test/ld-card.spec.tsx
+++ src/liquid/components/ld-card/test/ld-card.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing'
 import { LdCard } from '../ld-card'
+import { h } from '@stencil/core'
 
 describe('ld-card', () => {
   it('renders', async () => {
@@ -33,6 +34,41 @@ describe('ld-card', () => {
         html: `<ld-card shadow-interactive="sticky">Hello</ld-card>`,
       })
       expect(page.root).toHaveClass('ld-card--interactive-sticky')
+    })
+  })
+
+  describe('as listitem', () => {
+    it('adds listitem role if in card stack', async () => {
+      const page = await newSpecPage({
+        components: [LdCard],
+        template: () => (
+          <ld-card-stack>
+            <ld-card>Card A</ld-card>
+          </ld-card-stack>
+        ),
+      })
+      expect(page.body.querySelector('ld-card').getAttribute('role')).toEqual(
+        'listitem'
+      )
+    })
+
+    it('does not add listitem role if not in card stack', async () => {
+      const page = await newSpecPage({
+        components: [LdCard],
+        template: () => (
+          <div>
+            <ld-card>Card A</ld-card>
+          </div>
+        ),
+      })
+      expect(
+        page.body.querySelector('ld-card').getAttribute('role')
+      ).not.toEqual('listitem')
+    })
+
+    it('does not throw if there is no parent element', async () => {
+      const component = new LdCard()
+      component.componentWillLoad()
     })
   })
 })
