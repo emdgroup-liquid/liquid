e01c41331479c65c6bd14758765141bb1e978abb
borisdiakur <borisdiakur@gmail.com>
2023-10-02 13:41:04 +0000

    Deploying to gh-pages from @ emdgroup-liquid/liquid@1e7a74853e1b7bedbaf7d7f67ffebc100aa3f08f 🚀

--- 1695888377764/dist/build/p-82e9a1aa.js
+++
@@ -1,2 +0,0 @@
-const c=(o,s)=>s&&(s.closest(o)||c(o,s.getRootNode().host));export{c};
-//# sourceMappingURL=p-82e9a1aa.js.map
---
+++ dist/build/p-33f25dff.js
@@ -0,0 +1,2 @@
+const s=(c,o)=>o&&("closest"in o&&o.closest(c)||s(c,o.getRootNode().host));export{s as c};
+//# sourceMappingURL=p-33f25dff.js.map
