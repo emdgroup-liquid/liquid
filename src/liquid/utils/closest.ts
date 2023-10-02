1e7a74853e1b7bedbaf7d7f67ffebc100aa3f08f
Boris Diakur <borisdiakur@gmail.com>
2023-10-02 13:26:54 +0200

    fix(closest): account for closest method being undefined on document

--- src/liquid/utils/closest.ts
+++ src/liquid/utils/closest.ts
@@ -1,9 +1,9 @@
 // This helper function is similar to Element.closest(),
 // however it also traverses shadow DOM boundaries.
-export const closest = (selector: string, el: Element) => {
+export const closest = (selector: string, el: Element | Document) => {
   return (
     el &&
-    (el.closest(selector) ||
+    (('closest' in el && el.closest(selector)) ||
       closest(selector, (el.getRootNode() as unknown as ShadowRoot).host))
   )
 }
