31d1a4db119786f985976018ab2a892d114e1842
Boris Diakur <borisdiakur@gmail.com>
2023-10-11 12:22:17 +0200

    test(ld-file-upload): use template to pass props to component

--- src/liquid/components/ld-file-upload/ld-upload-progress/test/ld-upload-progress.spec.tsx
+++ src/liquid/components/ld-file-upload/ld-upload-progress/test/ld-upload-progress.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing'
 import { LdUploadProgress } from '../ld-upload-progress'
+import { h } from '@stencil/core'
 
 describe('ld-upload-progress', () => {
   it('renders default', async () => {
@@ -15,15 +16,21 @@ describe('ld-upload-progress', () => {
   it('renders list item', async () => {
     const page = await newSpecPage({
       components: [LdUploadProgress],
-      html: `<ld-upload-progress uploadItems=[
-        {
-          state: 'uploading',
-          fileName: 'file1.png',
-          fileSize: 100000,
-          fileType: 'png',
-          progress: 50,
-        },]>
-        </ld-upload-progress>`,
+      template: () => (
+        <ld-upload-progress
+          uploadItems={[
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            {
+              state: 'uploading',
+              fileName: 'file1.png',
+              fileSize: 100000,
+              fileType: 'png',
+              progress: 50,
+            },
+          ]}
+        ></ld-upload-progress>
+      ),
     })
     expect(page.root).toMatchSnapshot()
   })
